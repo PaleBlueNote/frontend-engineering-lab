5c0842e09d4b5c359031635694572f148796c953
Yoon seokchan <ysc0731@ajou.ac.kr>
2026-01-19 12:25:55 +0900

    feat: PWA 아이콘 에셋 등록 및 i

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'], // 캐싱할 정적 파일
+      manifest: {
+        name: 'FE Performance Lab',
+        short_name: 'FE Lab',
+        description: '프론트엔드 성능 최적화 실험실',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            "src": "android/android-launchericon-192-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "android/android-launchericon-512-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "android/android-launchericon-512-512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
 })
